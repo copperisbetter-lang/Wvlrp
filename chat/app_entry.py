811354fccf3d5409d206edb09f3019ef0ba1be44
copperisbetter-lang <copperisbetter@gmail.com>
2026-09-19 10:45:57 -0400

    Enable Hatch ranger routes

--- chat/app_entry.py
+++ chat/app_entry.py
@@ -1,3 +1,4 @@
 from inbox import app
 import ptz_routes  # registers protected East Bank PTZ routes
 import analytics  # registers persistent site and video analytics routes
+import hatch  # Hatch wildlife ranger, sightings, games, and camera-request queue\n
